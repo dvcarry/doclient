8580b8652d8a7e4297cd24bf7302d3ca9c50b4b4
Kirill <dvcarry@gmail.com>
2022-09-05 09:32:43 +0300

    change url

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,8 +3,8 @@ import { getAuthToken } from './helpers'
 
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5002/api/'
-    // baseURL: 'http://82.146.40.11:5002/api/'
+    // baseURL: 'http://localhost:5002/api/'
+    baseURL: 'http://82.146.40.11:5002/api/'
 });
 
 instance.defaults.headers.common = { 'Authorization': `Bearer ${getAuthToken()}` }
